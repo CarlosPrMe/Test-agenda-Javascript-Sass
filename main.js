a74cdbaa0dfae76b59d2d8d9fb9598406b94ef65
CarlosPrMe <48566832+CarlosPrMe@users.noreply.github.com>
2019-05-20 18:17:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,433 @@
+const contacts = [
+    {
+        "name": "Piedad",
+        "surname": "Dunfee",
+        "email": "piedad.dunfee@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Elsie",
+        "surname": "Calvery",
+        "email": "elsie.calvery@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Rodger",
+        "surname": "Pye",
+        "email": "rodger.pye@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Mohammad",
+        "surname": "Pye",
+        "email": "mohammad.pacheo@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Emmie",
+        "surname": "Derryberry",
+        "email": "emmie.derryberry@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Lino",
+        "surname": "Kottwitz",
+        "email": "lino.kottwitz@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Brittney",
+        "surname": "Ruddock",
+        "email": "brittney.ruddock@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "familia"
+    },
+    {
+        "name": "Jaimie",
+        "surname": "Human",
+        "email": "jaimie.human@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Darleen",
+        "surname": "Kesten",
+        "email": "darleen.kesten@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Georgie",
+        "surname": "Hannold",
+        "email": "georgie.hannold@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "familia"
+    },
+    {
+        "name": "Doreatha",
+        "surname": "Wheeler",
+        "email": "doreatha.wheeler@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Christie",
+        "surname": "Solum",
+        "email": "christie.solum@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Carlita",
+        "surname": "Whitmire",
+        "email": "carlita.whitmire@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "familia"
+    },
+    {
+        "name": "Luciana",
+        "surname": "Jobe",
+        "email": "luciana.jobe@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Sade",
+        "surname": "Selig",
+        "email": "sade.selig@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Devon",
+        "surname": "Goodridge",
+        "email": "devon.goodridge@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Bruna",
+        "surname": "Starkes",
+        "email": "bruna.starkes@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Glynis",
+        "surname": "Hudnall",
+        "email": "glynis.hudnall@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Anissa",
+        "surname": "Leggett",
+        "email": "anissa.leggett@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "familia"
+    },
+    {
+        "name": "Moshe",
+        "surname": "Moxley",
+        "email": "moshe.moxley@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Shannon",
+        "surname": "Olivieri",
+        "email": "shannon.olivieri@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Janel",
+        "surname": "Vandever",
+        "email": "janel.vandever@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "favoritos"
+    },
+    {
+        "name": "Tashia",
+        "surname": "Fontes",
+        "email": "tashia.fontes@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Merrilee",
+        "surname": "Lynde",
+        "email": "merrilee.lynde@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Coral",
+        "surname": "Eastin",
+        "email": "coral.eastin@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Shandi",
+        "surname": "Hunsaker",
+        "email": "shandi.hunsaker@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    },
+    {
+        "name": "Wilburn",
+        "surname": "Meyer",
+        "email": "wilburn.meyer@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Quintin",
+        "surname": "Schrom",
+        "email": "quintin.schrom@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Claribel",
+        "surname": "Sipes",
+        "email": "claribel.sipes@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "trabajo"
+    },
+    {
+        "name": "Nevada",
+        "surname": "Chappelle",
+        "email": "nevada.chappelle@domain.com",
+        "country": "españa",
+        "phone": "607 123 456",
+        "group": "amigos"
+    }
+]
+
+
+//---------------------------------------
+
+//funcion limpiar listado
+
+let user_list = document.getElementsByClassName('user-list')[0];
+function clear_list() {
+    user_list.innerHTML = ''
+}
+
+//---------------------------------------
+
+//FUNCION ORDENAR USUARIOS
+
+function user_sort(array) {
+    return array.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    })
+}
+
+//user_sort(contacts);
+
+//--------------------------------------------
+
+//FUNCION PINTAR USUARIOS EN LISTA
+
+
+function list_users(array) {
+    clear_list();
+    for (let i = 0; i < array.length; i++) {
+        let btn = document.createElement('button');
+        btn.setAttribute('class', 'btn btn--delete');
+        let li = document.createElement('li');
+        li.innerText = `${array[i].name} ${array[i].surname}`
+        li.setAttribute('class', 'user-list__item');
+        li.appendChild(btn);
+        user_list.appendChild(li);
+    }
+}
+
+//FUNCION PINTAR DETALLES
+
+function print_details(user, array) {
+    let heading = document.getElementsByClassName('contact__heading')[0];
+    let sub_heading = document.getElementsByClassName('contact__heading--detail')[0];
+    let tel = document.getElementsByClassName('contact__detail')[1];
+    let email = document.getElementsByClassName('contact__detail')[0];
+    let avatar = document.getElementsByClassName('contact__media')[0];
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (`${array[i].name} ${array[i].surname}` === user) {
+            let inicial = user.substring(0, 1);
+            heading.textContent = user;
+            sub_heading.innerText = `${array[i].group}`;
+            tel.textContent = `${array[i].phone}`;
+            email.innerText = `${array[i].email}`;
+            avatar.textContent = inicial;
+            nombre.value = `${array[i].name}`;
+            surname.value = `${array[i].surname}`;
+            email.value = `${array[i].email}`;
+            telefono.value = `${array[i].phone}`;
+            pais.value = `${array[i].country}`;
+            grupo.value = `${array[i].group}`;
+            break;
+        }
+    }
+}
+
+
+/* nombre.value = `${array[i].surname}`;
+surname.value = `${array[i].surname}`;
+email.value = `${array[i].email}`;
+telefono.value = `${array[i].phone}`;
+pais.value = `${array[i].country}`;
+grupo.value = `${array[i].group}`; */
+
+
+//--------------------------------------
+
+//FILTRAR POR GRUPO
+
+function getGroup(grupo, array) {
+    let sort = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].group === grupo) {
+            sort.push(array[i]);
+        }
+    }
+    return sort;
+}
+
+
+//--------------------------------------
+
+//SACAR ID DE LA LISTA
+
+let groups = document.getElementsByClassName('navbar')[0];
+groups.addEventListener('click', function (event) {
+
+    if (event.target.id === 'all') {
+        let orden = user_sort(contacts);
+        list_users(orden);
+    } else {
+        let agrupado = getGroup(event.target.id, contacts);
+        let ordenado = user_sort(agrupado);
+        list_users(ordenado);
+    }
+
+    //event.target.setAttribute('class', 'selected');
+    //event.target.classList.add('selected')
+
+})
+
+
+let lista = document.getElementsByClassName('user-list')[0];
+lista.addEventListener('click', (event) => {
+    let user = event.target.innerText;
+    print_details(user, contacts);
+})
+
+//RECOGER DATOS DEL FORMULARIO
+
+let nombre = document.getElementById('name');
+let apellidos = document.getElementById('surname');
+let email = document.getElementById('email');
+let pais = document.getElementById('phone-pre');
+let telefono = document.getElementById('phone-number');
+let grupo = document.getElementById('group');
+
+let add = document.getElementById('add_user');
+
+add.addEventListener('click', function () {
+
+    let new_user = {};
+    new_user.name = nombre.value;
+    new_user.surname = surname.value;
+    new_user.email = email.value;
+    new_user.phone = telefono.value;
+    new_user.country = pais.value;
+    new_user.group = grupo.value;
+    contacts.push(new_user);
+    nombre.value = '';
+    surname.value = '';
+    email.value = '';
+    telefono.value = '';
+    pais.value = '';
+    grupo.value = '';
+
+})
+
+
+//ABRIR VENTANA PARA AÑADIR MOSTRAR BOTON CORRESPONDIENTE
+
+$('.btn--more').click(function () {
+    $(this).css('display', 'none');
+    $('.menu').toggleClass('show');
+    $('#add_user').css('display', 'inline-block');
+
+})
+//ABRIR VENTANA PARA EDITAR
+
+$('#edit').click(function () {
+    $(this).css('display', 'none');
+    $('.menu').toggleClass('show');
+    $('#update_user').css('display', 'inline-block');
+})
+
+//OCULTAR VENTANA
+
+$('.btn--close').click(function () {
+    $('.btn--more').css('display', 'block');
+    $('#edit').css('display', 'block');
+    $('#update_user').css('display','none');
+    $('.menu').toggleClass('show');
+    $('#add_user').css('display', 'none');
+})
+
+
+$('.btn--delete').click(function () {
+    console.log('hola');
+    
+})
+
